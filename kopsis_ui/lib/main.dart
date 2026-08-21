fe386f40d44c454da891df558577694276809342
azkaawwabii <azkaawwabi123@gmail.com>
2026-08-21 07:46:39 +0700

    LKM-2 tata letak

---
+++ kopsis_ui/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Koperasi Sekolah')),
+        body: Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text('Buku Tulis'),
+              Row(
+                children: const [
+                  Text('Anggota: Rp3.000'),
+                  Text('Umum: Rp3.500'),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
